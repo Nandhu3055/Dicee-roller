c998fc3068447a548e74a594d5ea04c9c15ec33b
Nandhu3055 <127671120+Nandhu3055@users.noreply.github.com>
2023-07-03 21:35:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+var randomNumber1 = Math.floor((Math.random() * 6) + 1);
+var randomNumber2 = Math.floor((Math.random() * 6) + 1);
+if(randomNumber1>randomNumber2){
+    document.querySelector("h1").textContent = "Player 1 wins!!";
+}
+else if(randomNumber1 == randomNumber2){
+    document.querySelector("h1").textContent = "Draw";   
+}
+else{
+    document.querySelector("h1").textContent = "Player 2 wins!!";   
+}
+document.querySelector(".img1").src = "./images/dice"+randomNumber1+".png";
+document.querySelector(".img2").src = "./images/dice"+randomNumber2+".png";
